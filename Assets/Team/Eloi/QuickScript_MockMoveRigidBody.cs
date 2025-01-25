8b6afc3d0413ab8f2104976dd30579455f1e348f
Eloi Strée <eloistree@gmail.com>
2025-01-25 03:23:05 +0100

    Add Gamepad bytes parsing

---
+++ Assets/Team/Eloi/QuickScript_MockMoveRigidBody.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class QuickScript_MockMoveRigidBody : MonoBehaviour
+{
+    public Rigidbody m_whatToMove;
+    public float m_moveForce=2;
+    public ForceMode m_forceMode = ForceMode.Impulse;
+
+    public Vector2 m_joystickMove;
+
+    public void SetLeftJoystick(Vector2 joystick)
+    {
+        m_joystickMove = joystick;
+    }
+
+    public void Update()
+    {
+        Vector3 direction = new Vector3(m_joystickMove.x, 0, m_joystickMove.y);
+        m_whatToMove.AddForce(direction * m_moveForce, m_forceMode);
+
+    }
+}
---
+++ Assets/Team/Eloi/QuickScript_MockMoveTransform.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class QuickScript_MockMoveTransform :MonoBehaviour {
+
+    public Transform m_whatToMove;
+    public float m_speed;
+
+    public Vector2 m_joystickMove;
+    
+
+    public void SetLeftJoystick(Vector2 joystick)
+    {
+        m_joystickMove = joystick;
+    }
+
+    public void Update()
+    {
+        m_whatToMove.position += new Vector3(m_joystickMove.x, 0, m_joystickMove.y) * m_speed * Time.deltaTime;
+        
+    }
+}
