77d52a218c7c13e784f6ad47b915b2712c9ca6b6
Jack-Kaller <damien.sailly@protonmail.com>
2025-01-25 17:48:47 +0100

    Feat: fire and bouncing bubble

---
+++ Assets/Team/Damien/Scripts/BubbleDestructor.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class BubbleDestructor : MonoBehaviour
+{
+
+    [SerializeField] float lifetime = 2.0f;
+
+    private float _timer;
+    void Start()
+    {
+        _timer = lifetime;
+    }
+
+    void Update()
+    {
+        if (_timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _timer -= Time.deltaTime;    
+        }
+    }
+}
